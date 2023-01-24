11f792e322f223c656fafe46d88441883a1ec9e6
Byung jin <bjcho1503@naver.com>
2023-01-24 23:09:28 +0900

    폰트 적용 & 테스트

---
+++ Fine/Fine/Fonts/FontNames.swift
@@ -0,0 +1,11 @@
+/*
+ S-CoreDream-1Thin
+ name: S-CoreDream-2ExtraLight
+ name: S-CoreDream-4Regular
+ name: S-CoreDream-3Light
+ name: S-CoreDream-5Medium
+ name: S-CoreDream-6Bold
+ name: S-CoreDream-7ExtraBold
+ name: S-CoreDream-8Heavy
+ name: S-CoreDream-9Black
+ */
--- Fine/Fine/ViewController.swift
+++ Fine/Fine/ViewController.swift
@@ -6,14 +6,37 @@
 //
 
 import UIKit
+import SnapKit
+import Then
 
 class ViewController: UIViewController {
+    private let testLabel = UILabel().then {
+        $0.text = "옳른 비밀번호를 입력해주세요 :)"
+        $0.font = UIFont(name: "S-CoreDream-6Bold", size: 10)
+        $0.textColor = UIColor.black
+        $0.numberOfLines = 2
+        $0.textAlignment = .left
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .white
     }
-
-
+    
+    override func viewDidLayoutSubviews() {
+        addSubviews()
+        makeConstratints()
+    }
 }
 
+extension ViewController {
+    private func addSubviews() {
+        view.addSubview(testLabel)
+    }
+    
+    private func makeConstratints() {
+        testLabel.snp.makeConstraints {
+            $0.center.equalToSuperview()
+        }
+    }
+}
