bf510d63e96b1d5df8d100bcc95547db3f061870
Byung jin <bjcho1503@naver.com>
2023-01-24 21:35:49 +0900

    프로젝트 생성

---
+++ Fine/Fine/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Fine
+//
+//  Created by 조병진 on 2023/01/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
